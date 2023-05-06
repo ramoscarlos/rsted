2ef119a8b02d366938f29402aa2a405b5675e0f8
ramoscarlos <ramoscarlos@mint.2b>
2023-05-05 21:31:49 -0600

    Added summary page

--- application.py
+++ application.py
@@ -15,7 +15,7 @@ from flaskext.helpers import render_html
 # handle relative path references by changing to project directory
 run_from = os.path.dirname(os.path.abspath(sys.argv[0]))
 if run_from != os.path.curdir:
-    os.chdir(run_from)
+  os.chdir(run_from)
 
 # create our little application :)
 app = Flask(__name__)
@@ -24,58 +24,71 @@ app.config.from_pyfile(os.environ.get('RSTED_CONF', 'settings.py'))
 
 
 def view_is_active(view_name):
-    if request.path == url_for(view_name):
-        return 'active'
-    return ''
+  if request.path == url_for(view_name):
+    return 'active'
+
+  return ''
 
 @app.context_processor
 def ctx_pro():
-    return {
-        'MEDIA_URL': '/static/',
-        'is_active': view_is_active
-    }
+  return {
+    'MEDIA_URL': '/static/',
+    'is_active': view_is_active
+  }
 
 @app.route("/")
 @render_html('index.html')
 def index():
-    yield 'js_params', {'theme': request.args.get('theme', '')}
+  yield 'js_params', {'theme': request.args.get('theme', '')}
 
 
 @app.route('/about/')
 def about():
-    return render_template('about.html')
+  return render_template('about.html')
 
 @app.route('/minimal/')
 def minimal():
-    if not request.script_root:
-        request.script_root = url_for('index', _external = True)
+  if not request.script_root:
+    request.script_root = url_for('index', _external = True)
 
-    return render_template('editor-bulma.html')
+  return render_template('editor-bulma.html')
+
+@app.route('/summary/')
+def summary():
+  bullets = request.args.get('bullets')
+
+  if not bullets:
+    bullets = 3
+
+  if not request.script_root:
+    request.script_root = url_for('index', _external = True)
+
+  return render_template('summary-bulma.html', bullets=bullets)
 
 @app.route('/srv/rst2html/', methods=['POST', 'GET'])
 def rst2html():
-    rst = request.form.get('rst', '')
-    theme = request.form.get('theme')
-    if theme == 'basic':
-        theme = None
-    html = _rst2html(rst, theme=theme)
-    return html
+  rst = request.form.get('rst', '')
+  theme = request.form.get('theme')
+  if theme == 'basic':
+    theme = None
+  html = _rst2html(rst, theme=theme)
+  return html
 
 @app.route('/srv/rst2pdf/', methods=['POST'])
 def rst2pdf():
-    rst = request.form.get('rst', '')
-    theme = request.form.get('theme')
-    if theme == 'basic':
-        theme = None
+  rst = request.form.get('rst', '')
+  theme = request.form.get('theme')
+  if theme == 'basic':
+    theme = None
 
-    pdf = _rst2pdf(rst, theme=theme)
-    responce = make_response(pdf)
-    responce.headers['Content-Type'] = 'application/pdf'
-    responce.headers['Content-Disposition'] = 'attachment; filename="rst.pdf"'
-    responce.headers['Content-Transfer-Encoding'] = 'binary'
-    return responce
+  pdf = _rst2pdf(rst, theme=theme)
+  responce = make_response(pdf)
+  responce.headers['Content-Type'] = 'application/pdf'
+  responce.headers['Content-Disposition'] = 'attachment; filename="rst.pdf"'
+  responce.headers['Content-Transfer-Encoding'] = 'binary'
+  return responce
 
 
 if __name__ == '__main__':
-    app.run(host=app.config.get('HOST', '0.0.0.0'),
-            port=app.config.get('PORT', 5123))
+  app.run(host=app.config.get('HOST', '0.0.0.0'),
+          port=app.config.get('PORT', 5123))
